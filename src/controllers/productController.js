5beceed2b9142d99026cc5501b71c2e23a8b88a6
Tien Dat <multivncraft@gmail.com>
2025-03-03 20:36:20 +0700

    finish product controls , reviewing cUrl tests

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,156 @@
 const Product = require("../models/product");
+const { mongoose } = require("mongoose");
 
+const getAllProduct = async (req, res) => {
+  try {
+    const products = await Product.find();
+    if (products?.length === 0) {
+      return res.status(404).json({ message: "No product found" });
+    }
+    res.status(200).json({ products: products, isOk: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById({
+      _id: new mongoose.Types.ObjectId(id),
+    });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ product: product, isOk: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, stock, supplier, description, category } = req.body;
+    const product = new Product({
+      name: name,
+      description: description,
+      price: price,
+      stock: stock,
+      supplier: supplier,
+      category: category,
+    });
+    await product.save().then((newDoc) => {
+      console.log(newDoc);
+    });
+    if (!product) {
+      return res.status(400).json({ message: "Product not created" });
+    }
+    res.status(201).json({ product: product, isOk: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, stock, supplier, description, category } = req.body;
+    const product = await Product.findByIdAndUpdate(id, {
+      name: name,
+      description: description,
+      price: price,
+      stock: stock,
+      supplier: supplier,
+      category: category,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getPaginatedProducts = async (req, res) => {
+  try {
+    const { page, limit } = req.query;
+    const products = await Product.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+    const count = await Product.countDocuments();
+    res.status(200).json({
+      products: products,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      isOk: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted", isOk: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const searchProduct = async (req, res) => {
+  try {
+    const { name } = req.query;
+    const product = await Product.find({
+      name: { $regex: name, $options: "i" },
+    });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ product: product, isOk: true });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const filterProduct = async (req, res) => {
+  try {
+    const { category } = req.query;
+    const products = await Product.find({
+      category: { $regex: category, $options: "i" },
+    });
+    if (!products) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    return res.status(200).json({ products: products, isOk: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const sortProduct = async (req, res) => {
+  try {
+    const { sort } = req.query;
+    const products = await Product.find().sort({ price: sort });
+    if (!products) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    return res.status(200).json({ products: products, isOk: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
-    getAllProduct
-}
+  getAllProduct,
+  getProductById,
+  createProduct,
+  updateProduct,
+  getPaginatedProducts,
+  deleteProduct,
+  searchProduct,
+  filterProduct,
+  sortProduct,
+};
--- src/models/product.js
+++ src/models/product.js
@@ -1,34 +1,65 @@
 const mongoose = require("mongoose");
 const category = require("./category");
 
-
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        require: true,
-
+      type: String,
+      required: true,
+      validate: {
+        validator: async function (value) {
+          const product = await this.constructor.findOne({ name: value });
+          if (product) {
+            if (this.id === product.id) {
+              return true;
+            }
+            return false;
+          }
+          return true;
+        },
+        message: (props) => `Product name ${props.value} already exists`,
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value.length > 10;
+        },
+        message: "Description must be longer than 10 characters",
+      },
     },
     price: {
-        type: Number,
-        require: true,
+      type: Number,
+      required: true,
+      min: 0,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} is not an integer value",
+      },
     },
     stock: {
-        type: Number,
-        min: 0,
-        max: 100,
-        require: true,
-
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} is not an integer value",
+      },
     },
     supplier: {
-        type: String,
-        require: true,
+      type: String,
+      required: true,
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: category,
-        require: true,
-    }
-
-}, {timestamps: true})
+      type: String,
+      ref: category,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
